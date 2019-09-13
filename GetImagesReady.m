9fc22be23d7a8e2e7fb3a07d99d8e0168790b51c
Seral Buse Atak <44511062+heripotir@users.noreply.github.com>
2019-09-13 16:15:54 +0300

    Update GetImagesReady.m

--- GetImagesReady.m
+++ GetImagesReady.m
@@ -1,7 +1,14 @@
-
-for (i=1:33)
+%Get your images to ready for processing.
+%Ready: Files should be on Input with gray scale, their ground truth,
+%binarized versions should be on GT file with same name.
+%Following code will do it for you.
+%Change file names accordingly.
+%Careful for escape signs! '\\'
+%k=number of images
+k=33; 
+for (i=1:k)
    
-  a= imread(sprintf('C:\\Users\\Asus\\Desktop\\ooo\\img (%d).jpg',i));
+a= imread(sprintf('C:\\Users\\Asus\\Desktop\\ooo\\img (%d).jpg',i));
 b=rgb2gray(a);
 imwrite(b,sprintf('C:\\Users\\Asus\\Desktop\\bus-segmentation-master\\bus-segmentation-master\\vibot2013\\Data\\Input\\A(%d).png',i));
 
